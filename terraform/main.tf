5e2725fda8a3ad1820409f1af35629269b45f109
Mac <mac@Macs-MacBook-Pro.local>
2024-08-13 14:22:40 +0300

    added main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,8 @@ resource "azurerm_storage_account" "kowopesta" {
   location                 = azurerm_resource_group.kowoperg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
+  is_hns_enabled = true
+  account_kind = "StorageV2"
 
   tags = {
     environment = "staging"
